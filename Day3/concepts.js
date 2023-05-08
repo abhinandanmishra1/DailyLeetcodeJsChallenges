6a54a93f2d1916c9b6758c05417a591b36a7acdf
abhinandanmishra1 <abhinandanmishra360@gmail.com>
2023-05-09 00:48:10 +0530

    Day 3: LC Javascript challenge

---
+++ Day3/concepts.js
@@ -0,0 +1,15 @@
+// Understanding Object in javascript
+
+const width = 30;
+
+const add = (a, b) => a + b;
+
+const obj = {
+  color: "red",
+  height: 10,
+  "full name": "abhinandan mishra",
+  width,
+  add
+};
+
+console.log(obj, typeof obj);
---
+++ Day3/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+const obj = {
+    color: 'red',
+    height: 10,
+    'full name': 'abhinandan mishra',
+    width,  // width: width
+}
